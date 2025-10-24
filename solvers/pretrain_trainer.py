88c11c06d10b29d56a26d69d802d3444caa97f13
Damon Tingey <damon.tingey@gmail.com>
2025-10-24 17:00:41 -0600

    more cleanup

--- solvers/pretrain_trainer.py
+++ solvers/pretrain_trainer.py
@@ -1,26 +1,19 @@
 import os
 
-import matplotlib.pyplot as plt
-import sys
-
-from torch.cuda.amp import autocast
-
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
 import time
-from datetime import datetime
-import torch.distributed as dist
 import warnings
+
+from models.loss import HDNet_RCF_edge_criterion, cross_entropy_loss_RCF
+from utils.metric_utils import AverageMeter, points_detection_acc, update_acc
+from utils.poly_utils import 
 from utils.setting_utils import *
-from models.loss import *
 from utils.visualize import *
-from utils.poly_utils import *
-from utils.metric_utils import *
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 
 def Train_TCND(epoch, train_step, args, scaler):
@@ -33,23 +26,22 @@ def Train_TCND(epoch, train_step, args, scaler):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     countt = 0
-    recall = {'recall2': [], 'recall5': [], 'recall10': []}
-    precision = {'presicion2': [], 'presicion5': [], 'presicion10': []}
+    recall = {"recall2": [], "recall5": [], "recall10": []}
+    precision = {"presicion2": [], "presicion5": [], "presicion10": []}
 
     for i, batch in enumerate(args.train_loader):
         start = time.time()
         train_step += 1
-        heatmap = batch['heatmaps']
-        img = batch['images']
-        edge_map_true = batch['boundary_masks']
+        heatmap = batch["heatmaps"]
+        img = batch["images"]
+        edge_map_true = batch["boundary_masks"]
 
         image = img.to(device)
         pointmap = heatmap.to(device)
         edge_map_true = edge_map_true.to(device)
 
         # with autocast():
-        with torch.autocast(device_type='cuda', dtype=torch.float16):
-
+        with torch.autocast(device_type="cuda", dtype=torch.float16):
             outputs, feature_map = args.detection_model(image)
 
             if not isinstance(outputs, list):
@@ -58,12 +50,12 @@ def Train_TCND(epoch, train_step, args, scaler):
             gt = pointmap.unsqueeze(1)
             # gt = torch.cat([pointmap.unsqueeze(1), key_pointmap.unsqueeze(1)], dim=1)
             loss_node = args.detection_loss_function(outputs[-1], gt)
-            if args.configs['Model']['detection_model'] == 'SAM':
+            if args.configs["Model"]["detection_model"] == "SAM":
                 loss_edge = HDNet_RCF_edge_criterion(outputs[-2], edge_map_true)
                 loss = loss_node + loss_edge
             else:
                 loss_edge = torch.zeros(1).to(loss_node.device)
-                if args.configs['Model']['deep_supervision']:
+                if args.configs["Model"]["deep_supervision"]:
                     # loss_node += args.detection_loss_function(outputs[-2], pointmap.unsqueeze(1))
                     for o in outputs[:-1]:
                         loss_edge += cross_entropy_loss_RCF(o, edge_map_true)
@@ -71,7 +63,7 @@ def Train_TCND(epoch, train_step, args, scaler):
                 else:
                     loss = loss_node
 
-        batch_out = {'heatmap_pred': F.sigmoid(outputs[-1]),'outputs': [F.sigmoid(out) for out in outputs]}
+        batch_out = {"heatmap_pred": F.sigmoid(outputs[-1]), "outputs": [F.sigmoid(out) for out in outputs]}
 
         # backward
         args.optimizer_detection.zero_grad()
@@ -88,32 +80,41 @@ def Train_TCND(epoch, train_step, args, scaler):
         batch_time.update(time.time() - start)
 
         if i == 0:
-        # if i + 1:
-            print('Visualizing......')
+            # if i + 1:
+            print("Visualizing......")
             countt += 1
-            visualize_keypoints(batch, batch_out, epoch, vis_dir=args.vis_root, N=args.configs['Model']['NUM_POINTS'],
-                                mode='train', env=args)
-            if args.configs['Model']['show_outputs']:
-                visualize_sideouts(batch, batch_out, epoch=epoch, vis_dir=args.vis_root, mode='train')
-
-
+            visualize_keypoints(
+                batch,
+                batch_out,
+                epoch,
+                vis_dir=args.vis_root,
+                N=args.configs["Model"]["NUM_POINTS"],
+                mode="train",
+                env=args,
+            )
+            if args.configs["Model"]["show_outputs"]:
+                visualize_sideouts(batch, batch_out, epoch=epoch, vis_dir=args.vis_root, mode="train")
 
         # logger.write_hist_parameters(net=model,n_iter=train_step)
 
         if i % 10 == 0:
-            print('Epoch: {} [{}/{} ({:.2f}%)] | trainLoss:{:.6f}, node loss:{:.6f}, edge loss:{:.6f}'.format(
-                epoch, i + 1, len(args.train_loader), 100.0 * (i + 1) / len(args.train_loader), loss.item(),
-                loss_node.item(), loss_edge.item()))
+            print(
+                f"Epoch: {epoch} [{i + 1}/{len(args.train_loader)} ({100.0 * (i + 1) / len(args.train_loader):.2f}%)] | trainLoss:{loss.item():.6f}, node loss:{loss_node.item():.6f}, edge loss:{loss_edge.item():.6f}"
+            )
 
-    curr_detection_lr = args.optimizer_detection.state_dict()['param_groups'][0]['lr']
-    args.logger.write_scalars({
-        'TrainLoss': totalLosses.avg,
-        'NodeLoss': Losses_node.avg,
-        'EdgeLoss': Losses_edge.avg,
-        'point_lr': curr_detection_lr,
-    }, tag='train', n_iter=epoch)
+    curr_detection_lr = args.optimizer_detection.state_dict()["param_groups"][0]["lr"]
+    args.logger.write_scalars(
+        {
+            "TrainLoss": totalLosses.avg,
+            "NodeLoss": Losses_node.avg,
+            "EdgeLoss": Losses_edge.avg,
+            "point_lr": curr_detection_lr,
+        },
+        tag="train",
+        n_iter=epoch,
+    )
 
-    print('Train Epoch:{} | Loss: {:.4f}'.format(epoch, totalLosses.avg))
+    print(f"Train Epoch:{epoch} | Loss: {totalLosses.avg:.4f}")
 
     # for key, value in recall.items():
     #     print(key, ':', '{:.4f}'.format(sum(value) / len(value)))
@@ -129,22 +130,21 @@ def Val_TCND(epoch, val_step, best_loss, args):
         Losses_node = AverageMeter()
         Losses_edge = AverageMeter()
         countv = 0
-        recall = {'recall2': [], 'recall5': [], 'recall10': []}
-        precision = {'presicion2': [], 'presicion5': [], 'presicion10': []}
+        recall = {"recall2": [], "recall5": [], "recall10": []}
+        precision = {"presicion2": [], "presicion5": [], "presicion10": []}
 
         for i, batch in enumerate(args.val_loader):
             val_step += 1
-            heatmap = batch['heatmaps']
-            img = batch['images']
-            edge_map_true = batch['boundary_masks']
+            heatmap = batch["heatmaps"]
+            img = batch["images"]
+            edge_map_true = batch["boundary_masks"]
 
             image = img.to(device)
             pointmap = heatmap.to(device)
             edge_map_true = edge_map_true.to(device)
 
             # with autocast():
-            with torch.autocast(device_type='cuda', dtype=torch.float16):
-
+            with torch.autocast(device_type="cuda", dtype=torch.float16):
                 outputs, feature_map = args.detection_model(image)
 
                 if not isinstance(outputs, list):
@@ -153,12 +153,12 @@ def Val_TCND(epoch, val_step, best_loss, args):
                 gt = pointmap.unsqueeze(1)
                 # gt = torch.cat([pointmap.unsqueeze(1), key_pointmap.unsqueeze(1)], dim=1)
                 loss_node = args.detection_loss_function(outputs[-1], gt)
-                if args.configs['Model']['detection_model'] == 'SAM':
+                if args.configs["Model"]["detection_model"] == "SAM":
                     loss_edge = HDNet_RCF_edge_criterion(outputs[-2], edge_map_true)
                     loss = loss_node + loss_edge
                 else:
                     loss_edge = torch.zeros(1).to(loss_node.device)
-                    if args.configs['Model']['deep_supervision']:
+                    if args.configs["Model"]["deep_supervision"]:
                         # loss_node += args.detection_loss_function(outputs[-2], pointmap.unsqueeze(1))
                         for o in outputs[:-1]:
                             loss_edge += cross_entropy_loss_RCF(o, edge_map_true)
@@ -166,7 +166,7 @@ def Val_TCND(epoch, val_step, best_loss, args):
                     else:
                         loss = loss_node
 
-            batch_out = {'heatmap_pred': F.sigmoid(outputs[-1]), 'outputs': [F.sigmoid(out) for out in outputs]}
+            batch_out = {"heatmap_pred": F.sigmoid(outputs[-1]), "outputs": [F.sigmoid(out) for out in outputs]}
             # batch_out = {'heatmap_pred': F.sigmoid(outputs)}
 
             totalLosses.update(loss.item())
@@ -175,59 +175,74 @@ def Val_TCND(epoch, val_step, best_loss, args):
 
             # if epoch % 10 == 0 and epoch != 1:
             if epoch + 1:
-                vertices_pred, vertices_score = getPoints(outputs[-1], args.configs['Model']['NUM_POINTS'],
-                                                          get_score=True, gap=args.gap)
-                points_gt = batch['skel_points'] if args.configs['Experiment']['object_type'] == 'line' else batch[
-                    'polys']
+                vertices_pred, vertices_score = getPoints(
+                    outputs[-1], args.configs["Model"]["NUM_POINTS"], get_score=True, gap=args.gap
+                )
+                points_gt = (
+                    batch["skel_points"] if args.configs["Experiment"]["object_type"] == "line" else batch["polys"]
+                )
                 b_recall, b_precision = points_detection_acc(vertices_pred, points_gt)
 
                 recall, precision = update_acc(recall, precision, b_recall, b_precision)
 
             if i == 0 and (epoch - 1) % 3 == 0:
-                print('Visualizing......')
+                print("Visualizing......")
                 countv += 1
-                visualize_keypoints(batch, batch_out, epoch, vis_dir=args.vis_root,
-                                    N=args.configs['Model']['NUM_POINTS'], mode='val', env=args)
+                visualize_keypoints(
+                    batch,
+                    batch_out,
+                    epoch,
+                    vis_dir=args.vis_root,
+                    N=args.configs["Model"]["NUM_POINTS"],
+                    mode="val",
+                    env=args,
+                )
 
-                if args.configs['Model']['show_outputs']:
-                    visualize_sideouts(batch, batch_out, epoch=epoch, vis_dir=args.vis_root, mode='val')
+                if args.configs["Model"]["show_outputs"]:
+                    visualize_sideouts(batch, batch_out, epoch=epoch, vis_dir=args.vis_root, mode="val")
 
             # if i % int(len(env.val_loader) // 2) == 0:
             if i % 10 == 0:
-                print(
-                    'Evaluating [{}/{} ({:.2f}%)]'.format(i + 1, len(args.val_loader),
-                                                          100.0 * (i + 1) / len(args.val_loader)))
+                print(f"Evaluating [{i + 1}/{len(args.val_loader)} ({100.0 * (i + 1) / len(args.val_loader):.2f}%)]")
 
-        args.logger.write_scalars({
-            'ValLoss': totalLosses.avg,
-            'NodeLoss': Losses_node.avg,
-            'EdgeLoss': Losses_edge.avg,
-        }, tag='val', n_iter=epoch)
+        args.logger.write_scalars(
+            {
+                "ValLoss": totalLosses.avg,
+                "NodeLoss": Losses_node.avg,
+                "EdgeLoss": Losses_edge.avg,
+            },
+            tag="val",
+            n_iter=epoch,
+        )
 
-        print('Val Epoch:{} | valLoss: {:.4f}, Node Loss: {:.4f}, Edge Loss: {:.4f}'.format(epoch, totalLosses.avg, Losses_node.avg, Losses_edge.avg))
+        print(
+            f"Val Epoch:{epoch} | valLoss: {totalLosses.avg:.4f}, Node Loss: {Losses_node.avg:.4f}, Edge Loss: {Losses_edge.avg:.4f}"
+        )
         # if epoch % 10 == 0 and epoch != 1:
         if epoch + 1:
             for key, value in recall.items():
-                print(key, ':', '{:.4f}'.format(sum(value) / len(value)))
+                print(key, ":", f"{sum(value) / len(value):.4f}")
         #     for key, value in precision.items():
         #         print(key, ':', '{:.4f}'.format(sum(value) / len(value)))
-        acc = np.mean(recall['recall5'])
+        acc = np.mean(recall["recall5"])
         args.early_stopping([totalLosses, acc], best_loss, pretrain=True)
 
         if args.early_stopping.save_model:
-            best_loss['total_loss'] = totalLosses.avg
-            best_loss['acc'] = acc
+            best_loss["total_loss"] = totalLosses.avg
+            best_loss["acc"] = acc
             save_pretrain_model(args, best_loss, epoch)
-            pass
         if args.early_stopping.early_stop:
-            best_loss['early_stop'] = True
+            best_loss["early_stop"] = True
 
-        print('EarlyStopping counter: {} out of {}'.format(args.early_stopping.counter,
-                                                           args.early_stopping.patience))
+        print(f"EarlyStopping counter: {args.early_stopping.counter} out of {args.early_stopping.patience}")
         if args.early_stopping.counter % 2 == 0 and args.early_stopping.counter > 0:
-            print("lr from {} to {}".format(args.optimizer_detection.state_dict()['param_groups'][0]['lr'],
-                                            args.optimizer_detection.state_dict()['param_groups'][0]['lr'] * 0.5))
+            print(
+                "lr from {} to {}".format(
+                    args.optimizer_detection.state_dict()["param_groups"][0]["lr"],
+                    args.optimizer_detection.state_dict()["param_groups"][0]["lr"] * 0.5,
+                )
+            )
             for p in args.optimizer_detection.param_groups:
-                p['lr'] *= 0.5
+                p["lr"] *= 0.5
 
         return val_step, best_loss
--- utils/setting_utils.py
+++ utils/setting_utils.py
@@ -1,35 +1,35 @@
-import random
-import numpy as np
-import os
-import torch
-import torch.nn as nn
 import errno
-from collections import OrderedDict
-from jsmin import jsmin
 import json
+import os
+import random
+from collections import OrderedDict
+
+import numpy as np
+import torch
+from jsmin import jsmin  # type: ignore
+from torch import nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-def set_ddp(args, cfg, key='DDP'):
-    args.dist_url = cfg[key]['dist_url']
-    args.dist_backend = cfg[key]['dist_backend']
-    args.multiprocessing_distributed = cfg[key]['multiprocessing_distributed']
-    args.world_size = cfg[key]['world_size']
-    args.rank = cfg[key]['rank']
-    args.sync_bn = cfg[key]['sync_bn']
+def set_ddp(args, cfg, key="DDP"):
+    args.dist_url = cfg[key]["dist_url"]
+    args.dist_backend = cfg[key]["dist_backend"]
+    args.multiprocessing_distributed = cfg[key]["multiprocessing_distributed"]
+    args.world_size = cfg[key]["world_size"]
+    args.rank = cfg[key]["rank"]
+    args.sync_bn = cfg[key]["sync_bn"]
     return args
 
-def to_gpu(args, model, mode='DP'):
-    if mode == 'DP':
-          model = nn.DataParallel(model)
-          model = model.cuda()
-    elif mode == 'DDP':
+
+def to_gpu(args, model, mode="DP"):
+    if mode == "DP":
+        model = nn.DataParallel(model)
+        model = model.cuda()
+    elif mode == "DDP":
         if args.sync_bn:
             model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
-        model = nn.parallel.DistributedDataParallel(model.cuda(),
-                                                device_ids=[args.gpu],
-                                                find_unused_parameters=True)
+        model = nn.parallel.DistributedDataParallel(model.cuda(), device_ids=[args.gpu], find_unused_parameters=True)
     else:
         pass
 
@@ -37,8 +37,8 @@ def to_gpu(args, model, mode='DP'):
 
 
 def mkdirs(newdir):
-    """
-    make directory with parent path
+    """Make directory with parent path.
+
     :param newdir: target path
     """
     try:
@@ -57,7 +57,7 @@ def load_config(config_name="configs", config_dirpath="", try_default=False):
         config_filepath = os.path.join(config_dirpath, config_name + ".json")
 
     try:
-        with open(config_filepath, 'r') as f:
+        with open(config_filepath) as f:
             minified = jsmin(f.read())
             # config = json.loads(minified)
             try:
@@ -71,28 +71,25 @@ def load_config(config_name="configs", config_dirpath="", try_default=False):
         return config
     except FileNotFoundError:
         if config_name == "configs" and config_dirpath == "":
-            print(
-                "WARNING: the default configs file was not found....")
+            print("WARNING: the default configs file was not found....")
             return None
-        elif try_default:
+        if try_default:
             print(
-                "WARNING: configs file {} was not found, opening default configs file configs.defaults.json instead.".format(
-                    config_filepath))
+                f"WARNING: configs file {config_filepath} was not found, opening default configs file configs.defaults.json instead."
+            )
             return load_config()
-        else:
-            print(
-                "WARNING: configs file {} was not found.".format(config_filepath))
-            return None
+        print(f"WARNING: configs file {config_filepath} was not found.")
+        return None
 
 
 def build_roots(configs):
-    record_root = os.path.join('./records/', configs['Paths']['records_filename'])
+    record_root = os.path.join("./records/", configs["Paths"]["records_filename"])
     os.makedirs(record_root, exist_ok=True)
-    work_dir = '{}/{}'.format(os.path.join(record_root, 'runs'), 'Train')
+    work_dir = "{}/{}".format(os.path.join(record_root, "runs"), "Train")
     os.makedirs(work_dir, exist_ok=True)
-    vis_root = os.path.join(record_root, 'vis')
+    vis_root = os.path.join(record_root, "vis")
     os.makedirs(vis_root, exist_ok=True)
-    model_dir = os.path.join(record_root, 'weights')
+    model_dir = os.path.join(record_root, "weights")
     os.makedirs(model_dir, exist_ok=True)
     return work_dir, vis_root, model_dir, record_root
 
@@ -114,10 +111,10 @@ def load_ckpt(weight_dir, model):
     new_model_dict = OrderedDict()
 
     for k, v in pre_weight.items():
-        new_k = k.replace('module.', '') if 'module' in k else k
+        new_k = k.replace("module.", "") if "module" in k else k
         new_pre_weight[new_k] = v
     for k, v in model_dict.items():
-        new_k = k.replace('module.', '') if 'module' in k else k
+        new_k = k.replace("module.", "") if "module" in k else k
         new_model_dict[new_k] = v
 
     pre_weight = new_pre_weight  # ["model_state"]
@@ -135,25 +132,27 @@ def load_ckpt(weight_dir, model):
         else:
             unmatched_keys.append(k)
     # os._exit()
-    print(f'{v_n}/{t_n} weights have been loaded!')
-    print('Unmatched keys in state_dict:', unmatched_keys)
+    print(f"{v_n}/{t_n} weights have been loaded!")
+    print("Unmatched keys in state_dict:", unmatched_keys)
     model_dict.update(pretrained_dict)
     model.load_state_dict(model_dict)
 
     return model
 
-def load_swin_b_ckpt_for_TCSwin(weight_dir, model, load_range=[3,-43]):
+
+def load_swin_b_ckpt_for_TCSwin(weight_dir, model, load_range=[3, -43]):
     ckpt1 = torch.load(weight_dir)
     ckpt2 = model.state_dict()
     kl1 = list(ckpt1.keys())
-    for i, k in enumerate(list(ckpt2.keys())[load_range[0]:load_range[1]]):
+    for i, k in enumerate(list(ckpt2.keys())[load_range[0] : load_range[1]]):
         ckpt2[k] = ckpt1[kl1[i]]
     msg = model.load_state_dict(ckpt2, strict=False)
-    print(f'Load swin_transformer: {msg}')
+    print(f"Load swin_transformer: {msg}")
 
     return model
 
-def load_swin_b_ckpt(weight_dir, model, load_key='backbone'):
+
+def load_swin_b_ckpt(weight_dir, model, load_key="backbone"):
     pre_weight = torch.load(weight_dir)
     model_dict = model.state_dict()
     pre_weight_keys = list(pre_weight.keys())
@@ -161,9 +160,10 @@ def load_swin_b_ckpt(weight_dir, model, load_key='backbone'):
     for i, k in enumerate(backbone_keys):
         model_dict[k] = pre_weight[pre_weight_keys[i]]
     msg = model.load_state_dict(model_dict, strict=False)
-    print(f'Load swin_transformer: {msg}')
+    print(f"Load swin_transformer: {msg}")
 
     return model
+
 
 def setup_seed(seed):
     torch.manual_seed(seed)
@@ -174,57 +174,57 @@ def setup_seed(seed):
 
 
 def load_weight(args):
-    if args.configs['Experiment']['detection_resume']:
-        print('Loading Detection Model......')
-        detection_weight_path = args.configs['Paths']['pretrained_detection_weight_path']
+    if args.configs["Experiment"]["detection_resume"]:
+        print("Loading Detection Model......")
+        detection_weight_path = args.configs["Paths"]["pretrained_detection_weight_path"]
         # detection_weight_path = '/data02/ybn/Projects/TopDiG/records/Massachusetts/weights/points_best.pkl'
         args.detection_model = load_ckpt(detection_weight_path, args.detection_model)
-    if not args.configs['Experiment']['pretrain']:
-
-        print('Loading pretained ViT......')
-        ViT_weight_path = args.configs['Paths']['pretrained_ViT_weight_path']
+    if not args.configs["Experiment"]["pretrain"]:
+        print("Loading pretained ViT......")
+        ViT_weight_path = args.configs["Paths"]["pretrained_ViT_weight_path"]
         args.match_model = load_ckpt(ViT_weight_path, args.match_model)
 
-        if args.configs['Experiment']['match_resume']:
-            print('Loading Graph Model......')
-            match_weight_path = args.configs['Paths']['pretrained_match_weight_path']
+        if args.configs["Experiment"]["match_resume"]:
+            print("Loading Graph Model......")
+            match_weight_path = args.configs["Paths"]["pretrained_match_weight_path"]
             # match_weight_path = '/data02/ybn/Projects/TopDiG/records/Massachusetts/weights/connection_best.pkl'
             args.match_model = load_ckpt(match_weight_path, args.match_model)
     return args
 
+
 def save_model(self, best_loss, current_loss, epoch):
-    best_loss['total_loss'] = current_loss[0].avg
-    best_loss['point_loss'] = current_loss[1].avg
-    best_loss['match_loss'] = current_loss[2].avg
-    best_loss['boundary IoU'] = current_loss[3].avg
-    best_loss['mask IoU'] = current_loss[4].avg
-    if self.configs['Experiment']['detection_model_save']:
-        path_detection = os.path.join(self.model_dir,
-                                      'points_epoch{}_valLoss_{:.5f}_IoU_{:.4f}_ptloss_{:.4f}.pkl'.format(epoch, best_loss[
-                                          'total_loss'], best_loss['mask IoU'],best_loss['point_loss']))
+    best_loss["total_loss"] = current_loss[0].avg
+    best_loss["point_loss"] = current_loss[1].avg
+    best_loss["match_loss"] = current_loss[2].avg
+    best_loss["boundary IoU"] = current_loss[3].avg
+    best_loss["mask IoU"] = current_loss[4].avg
+    if self.configs["Experiment"]["detection_model_save"]:
+        path_detection = os.path.join(
+            self.model_dir,
+            "points_epoch{}_valLoss_{:.5f}_IoU_{:.4f}_ptloss_{:.4f}.pkl".format(
+                epoch, best_loss["total_loss"], best_loss["mask IoU"], best_loss["point_loss"]
+            ),
+        )
         torch.save(self.detection_model.state_dict(), path_detection)
-        print('Saving Detection Model=>', path_detection)
-    if self.configs['Experiment']['match_model_save']:
-        path_match = os.path.join(self.model_dir,
-                                  'connection_epoch{}_valLoss_{:.5f}_IoU_{:.4f}_matchloss_{:.5f}.pkl'.format(epoch,
-                                                                                                  best_loss[
-                                                                                                      'total_loss'],
-                                                                                                  best_loss[
-                                                                                                      'mask IoU'],
-                                                                                                  best_loss[
-                                                                                                      'match_loss']))
+        print("Saving Detection Model=>", path_detection)
+    if self.configs["Experiment"]["match_model_save"]:
+        path_match = os.path.join(
+            self.model_dir,
+            "connection_epoch{}_valLoss_{:.5f}_IoU_{:.4f}_matchloss_{:.5f}.pkl".format(
+                epoch, best_loss["total_loss"], best_loss["mask IoU"], best_loss["match_loss"]
+            ),
+        )
         torch.save(self.match_model.state_dict(), path_match)
-        print('Saving Graph Model=>', path_match)
+        print("Saving Graph Model=>", path_match)
 
     return best_loss
 
+
 def save_pretrain_model(args, best_loss, epoch):
     # best_iou = ious_mask.avg
-    if args.configs['Experiment']['detection_model_save']:
-        path_detection = os.path.join(args.model_dir,
-                                      'Pretrain_Epoch{}_valLoss_{:.4f}.pkl'.format(epoch, best_loss['total_loss']))
+    if args.configs["Experiment"]["detection_model_save"]:
+        path_detection = os.path.join(
+            args.model_dir, "Pretrain_Epoch{}_valLoss_{:.4f}.pkl".format(epoch, best_loss["total_loss"])
+        )
         torch.save(args.detection_model.state_dict(), path_detection)
-        print('Saving Model to =>', path_detection)
-
-
-
+        print("Saving Model to =>", path_detection)
